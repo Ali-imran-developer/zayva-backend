a94efb53e3e190bdcc93d69f4f7a4a0728431347
Ali-imran-developer <ai8440018@gmail.com>
2025-07-22 20:01:11 +0500

    fixing the issue of mongodb

--- api/index.js
+++ api/index.js
@@ -20,7 +20,6 @@ dbConnect();
 app.use(express.json());
 app.use(cors({
   origin : "https://zayva.vercel.app",
-  // origin : "http://localhost:5173",
   methods: ["GET", "POST", "DELETE", "PUT"],
   allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Expires", "Pragma",],
   credentials: true,
@@ -39,9 +38,9 @@ app.use("/api/shop/search", shopSearchRouter);
 app.use("/api/shop/review", shopReviewRouter);
 app.use("/api/common/feature", commonFeatureRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server is running on port ${process.env.PORT}`);
+// });
 
 module.exports = app;
 module.exports.handler = serverless(app);
