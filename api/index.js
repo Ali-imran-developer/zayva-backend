180f7558a48c855376ef522856d33bef18b0167a
Ali-imran-developer <ai8440018@gmail.com>
2025-07-22 19:51:41 +0500

    add the local connection

--- api/index.js
+++ api/index.js
@@ -20,6 +20,7 @@ dbConnect();
 app.use(express.json());
 app.use(cors({
   origin : "https://zayva.vercel.app",
+  // origin : "http://localhost:5173",
   methods: ["GET", "POST", "DELETE", "PUT"],
   allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Expires", "Pragma",],
   credentials: true,
@@ -37,6 +38,10 @@ app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
 app.use("/api/shop/review", shopReviewRouter);
 app.use("/api/common/feature", commonFeatureRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
